93f375bf2df2d8d1533771377d6fbbb7e86e51c6
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-11-29 13:13:09 +0100

    fix: Rotate increment in deadreckoning

--- romea_mobile_base_controllers/src/dead_reckoning.cpp
+++ romea_mobile_base_controllers/src/dead_reckoning.cpp
@@ -14,6 +14,7 @@
 
 
 // romea
+#include <cmath>
 #include <romea_core_common/math/EulerAngles.hpp>
 
 // local
@@ -55,8 +56,14 @@ void DeadReckoning::update(
 {
   if (previous_update_time_.has_value()) {
     double dt = (time - *previous_update_time_).seconds();
-    x_ = x_ + previous_longitudinal_speed_ * dt;
-    y_ = y_ + previous_lateral_speed_ * dt;
+    double dx = previous_longitudinal_speed_ * dt;
+    double dy = previous_lateral_speed_ * dt;
+    double ch = std::cos(heading_);
+    double sh = std::sin(heading_);
+
+    // dx and dy are rotated using the previous heading state
+    x_ = x_ + dx * ch - dy * sh;
+    y_ = y_ + dx * sh + dy * ch;
     heading_ = core::between0And2Pi(heading_ + previous_angular_speed_ * dt);
   }
 
